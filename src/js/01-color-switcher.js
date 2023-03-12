a19274f506721b1a04f85081eb28ad6b8be06901
PawelMajewskiPolska <114810121+PawelMajewskiPolska@users.noreply.github.com>
2023-03-12 16:15:09 +0100

    color switcher - finished

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,21 @@
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+const bodyColor = document.querySelector('body');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const newColor = () => {
+  timerId = setInterval(() => {
+    bodyColor.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startButton.disabled = true;
+};
+
+const stopColor = () => {
+  clearInterval(timerId);
+  startButton.disabled = false;
+};
+
+startButton.addEventListener('click', newColor);
+stopButton.addEventListener('click', stopColor);
