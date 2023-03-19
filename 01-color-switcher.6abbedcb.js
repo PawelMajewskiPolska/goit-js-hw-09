b2e67090cb42d6e31383a291a1f4a5dd305bbe91
PawelMajewskiPolska <114810121+PawelMajewskiPolska@users.noreply.github.com>
2023-03-19 17:22:49 +0000

    Deploying to gh-pages from @ PawelMajewskiPolska/goit-js-hw-09@b94b1c51b71aa2b0375ed4b4360fd3cc5e86ad0e 🚀

---
+++ 01-color-switcher.6abbedcb.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");t.addEventListener("click",(function(){var o=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3);t.disabled=!0;e.addEventListener("click",(function(){clearInterval(o),t.disabled=!1}))}))}();
+//# sourceMappingURL=01-color-switcher.6abbedcb.js.map
--- 01-color-switcher.95b3e19f.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]"),e=document.querySelector("body");t.addEventListener("click",(function(){setInterval((function(){e.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3);t.disabled=!0})),o.addEventListener("click",stopColor)}();
-//# sourceMappingURL=01-color-switcher.95b3e19f.js.map
